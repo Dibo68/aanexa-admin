9df6259744c1b2ecf2e98a342a117d20ac5d4749
Dibo68 <dibo68@gmail.com>
2025-07-15 16:44:12 -0500

    Create layout.tsx

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+// src/app/dashboard/layout.tsx
+import ProtectedRoute from '@/components/ProtectedRoute'
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ProtectedRoute>
+      {children}
+    </ProtectedRoute>
+  )
+}
